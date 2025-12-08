b746a0e8e08b719a39f7ad647409935ef5175ec8
jelinjon <jonasjelinek1@gmail.com>
2025-12-08 13:32:13 +0100

    init

---
+++ js/themechange.js
@@ -0,0 +1,7 @@
+var themeChangeButton = document.getElementById("themechange-button");
+
+themeChangeButton.addEventListener("click", toggleDarkmode);
+export function toggleDarkmode() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
